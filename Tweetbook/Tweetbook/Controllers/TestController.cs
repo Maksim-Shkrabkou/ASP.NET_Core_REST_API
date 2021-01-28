e91fe3a2655d61f75e78d313f75c4be15953a401
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-28 00:38:56 -0500

    Application setup and Swagger configuration was added

---
+++ Tweetbook/Tweetbook/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tweetbook.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("api/user")]
+        public IActionResult Get()
+        {
+            return Ok(new {name = "Nick"});
+        }
+    }
+}
